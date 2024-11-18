7804de8a26c1aa7c5c5fd24f36fa9f6fd80ca50f
hyuunminn <132858983+moonhyeonmin@users.noreply.github.com>
2024-11-18 22:27:45 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 200 ms, Memory: 33372 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
@@ -0,0 +1,16 @@
+import math
+
+n = int(input())
+result = n
+
+for i in range(2, int(math.sqrt(n) + 1)):
+    if n % i == 0:
+        result -= result / i
+        while n % i == 0:
+            n /= i
+
+if n > 1:
+    result -= result / n
+
+print(int(result))
+        
