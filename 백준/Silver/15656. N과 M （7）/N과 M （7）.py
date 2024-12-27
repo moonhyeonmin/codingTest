4f0a5c23b5e3718749df85d671911a50948c2e43
hyuunminn <132858983+moonhyeonmin@users.noreply.github.com>
2024-12-27 23:02:10 +0900

    [Silver III] Title: N과 M (7), Time: 1968 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+res = [0] * m
+visited = [False] * (n + 1)
+
+
+
+def func(k):
+    if k == m:
+        print(*res)
+        return
+
+    for i in range(n):
+        res[k] = arr[i]
+        func(k + 1)
+        res[k] = 0
+
+func(0)
+
