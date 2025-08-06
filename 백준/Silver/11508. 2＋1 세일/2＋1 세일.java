5babd9a86c1c1148936122ae70d34e3cc9785622
hyuunminn <mhm0627@sju.ac.kr>
2025-08-06 11:29:25 +0900

    [Silver IV] Title: 2+1 세일, Time: 440 ms, Memory: 27380 KB -BaekjoonHub

---
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 3 != 2) {
+                res += arr[i];
+            }
+        }
+
+        System.out.println(res);
+    }
+}
