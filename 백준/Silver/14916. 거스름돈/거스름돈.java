2a33c956c6e45c937a8cf4b1b22b7403011ad103
hyuunminn <mhm0627@sju.ac.kr>
2025-08-06 10:07:51 +0900

    [Silver V] Title: 거스름돈, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        while (true) {
+            if (n % 5 == 0) {
+                cnt += n / 5;
+                System.out.println(cnt);
+                break;
+            } else {
+                n -= 2;
+                cnt++;
+            }
+
+            if (n < 0) {
+                System.out.println(-1);
+                break;
+            }
+        }
+    }
+}
