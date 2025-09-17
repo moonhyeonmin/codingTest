bff754b926debe8152903e45ed5fbbf72273aa98
hyuunminn <mhm0627@sju.ac.kr>
2025-09-17 10:10:18 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        
+        int total = brown + yellow;
+        for (int height = 3; height <= total; height++) {
+            if (total % height == 0) {
+                int width = total / height;
+                
+                if ((height - 2) * (width - 2) == yellow) {
+                    return new int[] {Math.max(width, height), Math.min(width, height)};
+                }
+            }
+        }
+        
+        return new int[] {};
+    }
+}
