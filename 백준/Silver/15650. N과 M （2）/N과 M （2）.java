7012627ec3307e4ae2f03da73c86470d1777e8c9
hyuunminn <mhm0627@sju.ac.kr>
2025-10-20 16:00:49 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 14584 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[] arr;
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        func(1, 0);
+
+    }
+
+    static void func(int now, int cnt) {
+        if (cnt == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = now; i <= n; i++) {
+            arr[cnt] = i;
+            func(i + 1, cnt + 1);
+        }
+    }
+}
